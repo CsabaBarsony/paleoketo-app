ca537d4a4d64fe6e5db084500c6deb3ee62a4801
CsabaBarsony <csaba.barsony@gmail.com>
2016-02-21 21:20:44 +0100

    remove React testing

--- tests/name.js
+++ tests/name.js
@@ -7,19 +7,14 @@ global.window = document.parentWindow;
 global._ = require('../public/js/lodash');
 global.bella = require('../public/js/bella');
 global.React = require('react/addons');
-global.ReactDOM = require('react-dom');
+//global.ReactDOM = require('react-dom');
 var TestUtils = React.addons.TestUtils;
 
 describe('name', function() {
-	var Home = require('../src/scripts/components/home/home');
 
 	it('valami', function() {
 		var server = require('../src/scripts/server');
 		var name = server.food.getName();
 		assert.equal(name, 'this is my name');
-	});
-
-	it('React testing...', function() {
-		var x = 0;
 	});
 });
